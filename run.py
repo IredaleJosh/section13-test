aea08a6b1220ad5123aaab643c77f3b76d12c72e
IredaleJosh <sc23ji@leeds.ac.uk>
2024-11-20 15:05:03 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from app import app
 
-#app.run(debug=True)
+# When Deploying - Ensure NOT calling app.run()
+# Why? will crash app and give 504 error, as not using Flask's built-in
+# server
+# PythonAnywhere uses WSGI instead
+
+# Makes it so only run when file itslef ran directly
+# Can run locally when using "flask run" command
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
